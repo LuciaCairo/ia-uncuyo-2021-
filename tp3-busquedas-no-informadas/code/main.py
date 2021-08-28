aabc65e6dcc466a06740feb2a3f248d23b998dc1
LuciaCairo <88351747+LuciaCairo@users.noreply.github.com>
2021-08-28 19:49:22 -0300

    Create main.py

---
+++ tp3-busquedas-no-informadas/code/main.py
@@ -0,0 +1,22 @@
+# A) Implementar un agente basado en objetivos que dado un punto de inicio y un punto destino, encuentre el camino óptimo.
+
+from Environment import *
+from Agent import *
+import random
+
+#for i in range (30):
+#    t = Environment()
+    #print(t.grilla)
+    #t.print_environment()
+#    a = Agent(t)
+#    print(len(a.BFS()))
+
+#for i in range (30):
+#    t = Environment()
+#    a = Agent(t)
+#    print(len(a.DFS(500000)))
+
+#for i in range (30):
+#    t = Environment()
+#    a = Agent(t)
+#    print(len(a.Uniform()))
